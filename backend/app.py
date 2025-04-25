bd7efe571179e87c1ccf9a715130e5762e89e309
MM031095 <maximemasson65@gmail.com>
2025-04-25 11:08:22 +0200

    feat(all): Intégre le traitement des images.

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import imgaug as ia
+from imgaug import augmenters as iaa
 import torch
 from torch.nn import functional as F
 from huggingface_hub import login, hf_hub_download
@@ -6,19 +8,25 @@ from flask import Flask, request, jsonify
 from PIL import Image
 from collections import namedtuple
 import numpy as np
+np.bool = np.bool_
+np.complex = np.complex_
 from matplotlib import colors
 import base64
 import io
 import os
 
-HF_TOKEN = os.environ.get("HUGGINGFACE_TOKEN")
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+#HF_TOKEN = os.environ.get("HUGGINGFACE_TOKEN")
+HF_TOKEN = "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"
 login(token=HF_TOKEN)
 
 # Model
-REPO_ID = "mmassonn/CarSegmentation"
+REPO_ID = "mmassonn/Segformer_tune"
 MODEL_FILE_NAME = "Segformer_tune.pt"
 model_file = hf_hub_download(repo_id=REPO_ID,filename=MODEL_FILE_NAME)
-MODEL = torch.load(model_file, map_localisation="cpu", weights_only=False)
+MODEL = torch.load(model_file, map_location="cpu", weights_only=False)
 
 # Dataset
 DATASET_IMAGE_REPO_ID = "mmassonn/CarSegmentation_leftImg8bit"
@@ -45,36 +53,88 @@ def get_dataset_file_path() -> tuple[list, dict]:
 MODEL_INPUT_WIDTH = 512
 MODEL_INPUT_HEIGHT = 512
 
+def load_augmentation_aug_geometric() -> iaa.OneOf:
+    """Applique des modifications géométriques des données dans le cadre de la data augmentation."""
+    return iaa.OneOf([
+        iaa.Sequential([iaa.Fliplr(0.2), iaa.Flipud(0)]),
+        iaa.CropAndPad(percent=(-0.1, 0.1), pad_mode='constant', pad_cval=(0, 255)),
+        iaa.Crop(percent=(0.0, 0.3)),
+        iaa.Crop(percent=(0.1, 0.5)),
+        iaa.Crop(percent=(0.2, 0.4)),
+        iaa.Crop(percent=(0.0, 0.25)),
+        iaa.Sequential([
+        iaa.Affine(
+                scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+                rotate=(-45, 45),
+                shear=(-16, 16),
+                order=[0, 1],
+                mode='constant',
+                cval=(0, 255),
+            )])
+    ])
 
-def generate_img_from_mask(mask, colors_palette=['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']):
-    """Genère une image à partir du masque de segmentation."""
+def load_augmentation_aug_non_geometric() -> iaa.OneOf:
+    """Applique des modifications non géométriques des données dans le cadre de la data augmentation."""
+    return iaa.OneOf([
+        iaa.Sometimes(0.2, iaa.GaussianBlur(sigma=(0, 3.0))),
+        iaa.Sometimes(0.2, iaa.MotionBlur(k=15, angle=[-45, 45])),
+        iaa.Sometimes(0.2, iaa.MultiplyHue((0.5, 1.5))),
+        iaa.Sometimes(0.34, iaa.Grayscale(alpha=(0.0, 1.0))),
+        iaa.Sometimes(0.1, iaa.HistogramEqualization()),
+        iaa.Sometimes(0.2, iaa.Rain(speed=(0.1, 0.2))),
+        iaa.Sometimes(0.2, iaa.Fog()),
+        iaa.Sometimes(0.2, iaa.Snowflakes(flake_size=(0.7, 0.95), speed=(0.001, 0.03)))
+    ])
 
-    id2category = {0: 'void',
-                   1: 'flat',
-                   2: 'construction',
-                   3: 'object',
-                   4: 'nature',
-                   5: 'sky',
-                   6: 'human',
-                   7: 'vehicle'}
-    img_seg = np.zeros((mask.shape[0], mask.shape[1], 3), dtype='float')
-    for cat in id2category.keys():
-        img_seg[:, :, 0] += mask[:, :, cat] * colors.to_rgb(colors_palette[cat])[0]
-        img_seg[:, :, 1] += mask[:, :, cat] * colors.to_rgb(colors_palette[cat])[1]
-        img_seg[:, :, 2] += mask[:, :, cat] * colors.to_rgb(colors_palette[cat])[2]
+def load_augmentation_aug_all(test: bool = False) -> iaa.Sequential:
+    """Applique selon une probabilité des modifications géométriques et non géométriques des données dans le cadre de la data augmentation."""
+    if test:
+        return iaa.Sequential([
+        iaa.Sometimes(0.9, load_augmentation_aug_non_geometric()),
+        iaa.Sometimes(0.9, load_augmentation_aug_geometric())
+    ])
+    else:
+        return iaa.Sequential([
+            iaa.Sometimes(0.5, load_augmentation_aug_non_geometric()),
+            iaa.Sometimes(0.5, load_augmentation_aug_geometric())
+        ])
 
-    return img_seg
+def apply_augmentation_from_array(img_array):
+    """Applique la data augmentation sur l'image et le masque."""
+    augmentation_function = load_augmentation_aug_all(test=True)
+    aug_det = augmentation_function.to_deterministic()
+    return aug_det.augment_image(img_array)
+
+def get_numpy_mask_from_image(mask_img):
+    """Retourne un np.array avec 1 canaux, chacun représentant une caractéristique distincte."""
+    conditions = [
+    (mask_img >= 0) & (mask_img <= 6),
+    (mask_img >= 7) & (mask_img <= 10),
+    (mask_img >= 11) & (mask_img <= 16),
+    (mask_img >= 17) & (mask_img <= 20),
+    (mask_img >= 21) & (mask_img <= 22),
+    (mask_img == 23),
+    (mask_img >= 24) & (mask_img <= 25),
+    ((mask_img >= 26) & (mask_img <= 33)) | (mask_img == -1)
+    ]
+    values = [0, 1, 2, 3, 4, 5, 6, 7]
+    
+    return np.select(conditions, values, default=mask_img)
 
 def predict_segmentation(
-    image_array, 
+    image_path: str, 
     image_width: int, 
     image_height: int,
     ):
     '''Genère le masque de couleur à partir du modèle.'''
-    image_array = Image.fromarray(image_array).resize((image_width, image_height))
-    img_tensor = (torch.from_numpy(img).float().permute(2, 0, 1) / 255.0).unsqueeze(0)
+    image = Image.open(image_path)
+    image_array = np.array(image)
+    resized_image = image.resize((image_width, image_height))
+    resized_array = np.array(resized_image)
+    img_tensor = (torch.from_numpy(resized_array).float().permute(2, 0, 1) / 255.0).unsqueeze(0)
     with torch.no_grad():
-        outputs = model(pixel_values=img_tensor.float())
+        outputs = MODEL(pixel_values=img_tensor.float())
         logits = outputs.logits
         upsampled_logits = F.interpolate(
             logits,
@@ -96,9 +156,9 @@ def get_image_file_path():
     image_dataset_file_names, _ = get_dataset_file_path()
     return jsonify(image_dataset_file_names[1:])
 
-@app.route("/predict_mask", methods=['GET', 'POST'])
-def segment_image() -> list:
-    """Réalise la segmentation d'un images à partir de son path."""
+@app.route("/process_image", methods=['GET', 'POST'])
+def process_image() -> list:
+    """Réalise le prétraitement d'une image à partir de son path."""
     file = request.get_json(force=True)
     image_file_name = file['file_name']
     image_path = hf_hub_download(
@@ -108,13 +168,39 @@ def segment_image() -> list:
         )
     image = Image.open(image_path)
     image_array = np.array(image)
+    process_img = apply_augmentation_from_array(img_array=image_array)
+    # Convertir les arrays en images encodées en base64
+    def array_to_base64(arr):
+        if isinstance(arr, Image.Image):
+            img = arr
+        else:
+            img = Image.fromarray(arr.astype('uint8'))
+        buffered = io.BytesIO()
+        img.save(buffered, format="PNG")
+        return base64.b64encode(buffered.getvalue()).decode('utf-8')
 
-    pred_mask_array = predict_segmentation(
-        image_array=image_array,
+    return {
+        'image': array_to_base64(image_array),
+        'process_img': array_to_base64(process_img),
+    }
+
+@app.route("/predict_mask", methods=['GET', 'POST'])
+def segment_image() -> list:
+    """Réalise la segmentation d'une image à partir de son path."""
+    file = request.get_json(force=True)
+    image_file_name = file['file_name']
+    image_path = hf_hub_download(
+        repo_id=DATASET_IMAGE_REPO_ID,
+        filename=image_file_name,
+        repo_type="dataset",
+        )
+    image = Image.open(image_path)
+    image_array = np.array(image)
+    pred_mask_tensor = predict_segmentation(
+        image_path=image_path,
         image_width=MODEL_INPUT_WIDTH,
         image_height=MODEL_INPUT_HEIGHT,
         )
-    
     _ , dataset_file_paths = get_dataset_file_path()
     real_mask_file_name = dataset_file_paths[image_file_name]
     real_mask_path = hf_hub_download(
@@ -122,10 +208,10 @@ def segment_image() -> list:
         filename=real_mask_file_name,
         repo_type="dataset",
         )
+    pred_mask_array = pred_mask_tensor.squeeze(0).numpy()
+
     mask_array = np.array(Image.open(real_mask_path).convert('L'))
-    real_mask_array = get_numpy_mask_from_image(mask_img = mask_array)
-    real_mask_array_color = generate_img_from_mask(real_mask_array) * 255
-    
+    mask_7_labels_array = get_numpy_mask_from_image(mask_img = mask_array) 
     # Convertir les arrays en images encodées en base64
     def array_to_base64(arr):
         if isinstance(arr, Image.Image):
@@ -139,5 +225,5 @@ def segment_image() -> list:
     return {
         'image': array_to_base64(image_array),
         'pred_mask': array_to_base64(pred_mask_array),
-        'real_mask': array_to_base64(real_mask_array_color),
+        'real_mask': array_to_base64(mask_7_labels_array),
     }
--- frontend/interface.py
+++ frontend/interface.py
@@ -5,8 +5,12 @@ from PIL import Image
 import base64
 import io
 
-DATA_API_URL = "https://carsegmentationwebapp-bgggfnfgefdchrgp.francecentral-01.azurewebsites.net/image_path"
-PREDICTION_API_URL = "https://carsegmentationwebapp-bgggfnfgefdchrgp.francecentral-01.azurewebsites.net/predict_mask"
+#DATA_API_URL = "https://carsegmentationwebapp-bgggfnfgefdchrgp.francecentral-01.azurewebsites.net/image_path"
+#PREDICTION_API_URL = "https://carsegmentationwebapp-bgggfnfgefdchrgp.francecentral-01.azurewebsites.net/predict_mask"
+
+DATA_API_URL = "http://localhost:5000/image_path"
+PROCESSING_API_URL = "http://localhost:5000/process_image"
+PREDICTION_API_URL = "http://localhost:5000/predict_mask"
 
 def get_file_list_from_api():
     """Récupére de la liste des images disponibles depuis l'api."""
@@ -18,8 +22,19 @@ def get_file_list_from_api():
         st.error("Erreur lors de la récupération de la liste des fichiers.")
         return []
 
-def send_post_request(selected_file):
-    """Envoie à l'api le nom de l'image sélectionnée."""
+def send_post_request_processing(selected_file):
+    """Envoie à l'api pour le prétraitement, le nom de l'image sélectionnée."""
+    url = PROCESSING_API_URL
+    files = {'file_name': str(selected_file)}
+    response = requests.post(url, json=files)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        st.error("Erreur lors de l'envoi de la requête POST.")
+        return None
+
+def send_post_request_prediction(selected_file):
+    """Envoie à l'api pour la prédiction le nom de l'image sélectionnée."""
     url = PREDICTION_API_URL
     files = {'file_name': str(selected_file)}
     response = requests.post(url, json=files)
@@ -29,7 +44,46 @@ def send_post_request(selected_file):
         st.error("Erreur lors de l'envoi de la requête POST.")
         return None
 
-def display_images(images, target_size=(300, 300)):
+
+# Définir votre palette de couleurs personnalisée (7 couleurs max)
+colors_palette = [
+    [0, 0, 255],    # Bleu (b)
+    [0, 255, 0],    # Vert (g)
+    [255, 0, 0],    # Rouge (r)
+    [0, 255, 255],  # Cyan (c)
+    [255, 0, 255],  # Magenta (m)
+    [255, 255, 0],  # Jaune (y)
+    [110, 50, 30],  # Marron (y)
+    [0, 0, 0]       # Noir (k)
+]
+colors_array = np.array(colors_palette, dtype=np.uint8)
+
+def apply_custom_cmap(array):
+    """Applique la palette de couleurs à un array 2D"""
+    normalized = (array - array.min()) * (len(colors_array)-1) / (array.max() - array.min() + 1e-10)
+    indices = np.clip(normalized.astype(int), 0, len(colors_array)-1)
+    return colors_array[indices]
+
+def display_process_images(images, target_size=(512, 512)):
+    """Affiche l'image réelle, le masque segmentée réelle et celui réalisé par le modèle."""
+    descriptions = {
+        'image': "Image avant transformation",
+        'process_img': "Image après transformation",
+    }
+
+    cols = st.columns(len(images))    
+    for idx, (key, base64_string) in enumerate(images.items()):
+        img_data = base64.b64decode(base64_string)
+        img = Image.open(io.BytesIO(img_data))
+        img_resized = img.resize(target_size, Image.LANCZOS)
+        array = np.array(img_resized)
+        # Conversion de type et application de la palette
+        if array.dtype != np.uint8:
+            array = (array * 255).astype(np.uint8)
+        with cols[idx]:
+            st.image(array, caption=descriptions[key], use_container_width=True)
+
+def display_images(images, target_size=(512, 512)):
     """Affiche l'image réelle, le masque segmentée réelle et celui réalisé par le modèle."""
     descriptions = {
         'image': "Image réelle",
@@ -44,22 +98,45 @@ def display_images(images, target_size=(300, 300)):
         img = Image.open(io.BytesIO(img_data))
         img_resized = img.resize(target_size, Image.LANCZOS)
         array = np.array(img_resized)
-        if array.dtype != np.uint8:
+        # Conversion de type et application de la palette
+        if array.ndim == 2:  # Si image grayscale
+            if array.dtype != np.uint8:
+                array = (array * 255).astype(np.uint8)
+            array = apply_custom_cmap(array)
+        elif array.dtype != np.uint8:
             array = (array * 255).astype(np.uint8)
         with cols[idx]:
             st.image(array, caption=descriptions[key], use_container_width=True)
 
-st.title("Application de Segmentation d'Image")
-
+st.title("Segmentation d'une scène urbaine.")
 file_list = get_file_list_from_api()
-selected_file = st.selectbox("Sélectionnez un fichier", file_list)
 
-if st.button("Création de l'image segmentée"):
-    if selected_file:
-        images = send_post_request(selected_file)
-        if images:
-            display_images(images)
+col1, col2 = st.columns(2)
+
+# Partie gauche : Prétraitement
+with col1:
+    st.header("Prétraitement d'une image")
+    selected_file_pret = st.selectbox("Sélectionnez un fichier à prétraiter", file_list, key="pret")
+    if st.button("Réaliser le prétraitement de l'image"):
+        if selected_file_pret:
+            images_pret = send_post_request_processing(selected_file_pret)
+            if images_pret:
+                display_process_images(images_pret)
+            else:
+                st.error("Erreur lors du prétraitement de l'image sélectionnée.")
         else:
-            st.error("Erreur lors de la récupération du fichier sélectionné.")
-    else:
-        st.warning("Veuillez sélectionner un fichier.")
+            st.warning("Veuillez sélectionner un fichier.")
+
+# Partie droite : Prédiction
+with col2:
+    st.header("Réaliser la prédiction")
+    selected_file_pred = st.selectbox("Sélectionnez un fichier à prédire", file_list, key="pred")
+    if st.button("Création de l'image segmentée"):
+        if selected_file_pred:
+            images_pred = send_post_request_prediction(selected_file_pred)
+            if images_pred:
+                display_images(images_pred)
+            else:
+                st.error("Erreur lors de la récupération du fichier sélectionné.")
+        else:
+            st.warning("Veuillez sélectionner un fichier.")
